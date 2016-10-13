ea2c011bdd98a786458b3882c33be5a68e753ced
Tim Hoolihan <tim.hoolihan@dialogtech.com>
2016-10-13 07:49:42 -0400

    Bag of words created

--- explore.py
+++ explore.py
@@ -5,6 +5,8 @@ from bs4 import BeautifulSoup
 import re
 import nltk
 from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
 
 # load
 train = pd.read_csv(project.labeled, header=0, delimiter="\t", quoting=csv.QUOTE_NONE)
@@ -35,4 +37,27 @@ def tokenize_review(raw_review):
     return(" ".join(words))
 
 # test with first review
+print('First review example clean:')
 print(tokenize_review(train.review[0]))
+print('\n')
+
+# clean all reviews
+print("Cleaning all reviews...\n")
+train.review = train.review.map(tokenize_review)
+
+# create bag of words
+print("Creating bag of words...\n")
+vectorizer = CountVectorizer(analyzer="word", \
+                             tokenizer=None, \
+                             preprocessor=None, \
+                             stop_words=None, \
+                             max_features=5000)
+
+train_data_features = vectorizer.fit_transform(train.review).toarray()
+
+vocab = vectorizer.get_feature_names()
+freq = np.sum(train_data_features, axis=0)
+
+print("Examine bag of words...")
+for token, frq in zip(vocab, freq):
+    print("%s: %d" % (token, frq))
